5981a022f715f6c0ce33341478255bd29be6ed73
Adrian Szczęch <adrianszcz64@gmail.com>
2024-05-23 05:59:03 +0200

    Merge dataframes

--- kpi_data_processing_main.py
+++ kpi_data_processing_main.py
@@ -32,21 +32,27 @@ last_index_rozliczenie_miesieczne = df_rozliczenie_miesieczne['NR.PRODUKCYJNY'].
 df_rozliczenie_miesieczne = df_rozliczenie_miesieczne.iloc[:last_index_rozliczenie_miesieczne + 1, :36]
 
 # designation columns which are needed for df_rozliczenie_miesieczne
-df_rozliczenie_miesieczne_needed_columns_for_kpi = [0, 10, 23, 25]
+df_rozliczenie_miesieczne_needed_columns_for_kpi = [0, 10, 18, 23, 25]
 
 # create DataFrame obiect with collums needed for kpi analysis
 df_rozliczenie_miesieczne = df_rozliczenie_miesieczne.iloc[:, df_rozliczenie_miesieczne_needed_columns_for_kpi]
 
 # change data type for date in column 3
-df_rozliczenie_miesieczne.iloc[:, 3] = pd.to_datetime(df_rozliczenie_miesieczne.iloc[:, 3],dayfirst=True)
+df_rozliczenie_miesieczne.iloc[:, 4] = pd.to_datetime(df_rozliczenie_miesieczne.iloc[:, 4],dayfirst=True)
 
 # setting value of quantity correction mixers for 1 if value is higher than 1
 df_rozliczenie_miesieczne.iloc[:, 1] = df_rozliczenie_miesieczne.iloc[:, 1].apply(lambda x: x if x <= 1 else 1)
 
+# removing blank characters from column with index and batch number
+df_rozliczenie_miesieczne.iloc[:, 2] = df_rozliczenie_miesieczne.iloc[:, 2].apply(lambda x: str(x).strip())
+df_rozliczenie_miesieczne.iloc[:, 2] = df_rozliczenie_miesieczne.iloc[:, 2].apply(lambda x: str(x).strip()[-3:])
+df_rozliczenie_miesieczne.iloc[:, 3] = df_rozliczenie_miesieczne.iloc[:, 3].apply(lambda x: str(x).strip())
+df_rozliczenie_miesieczne.iloc[:, 3] = df_rozliczenie_miesieczne.iloc[:, 3].apply(lambda x: str(x).upper())
+
 
 # upload "Premix" sheet from korekty excel file to DatFrame object
 # remove first row from excel because have not needed informations (skiprows=1)
-df_korekty = pd.read_excel(filename_korekty, sheet_name="Premix",skiprows=1)
+df_korekty = pd.read_excel(filename_korekty, sheet_name="Premix",dtype={'Nr partii': str}, skiprows=1)
 
 # searching last fulfill element in collumn "index"
 last_index_korekty = df_korekty['index'].last_valid_index()
@@ -55,17 +61,43 @@ last_index_korekty = df_korekty['index'].last_valid_index()
 df_korekty = df_korekty.iloc[:last_index_korekty + 1, :18]
 
 # designation columns which are needed for df_korekty
-df_korekty_needed_columns_for_kpi = [2, 4]
+df_korekty_needed_columns_for_kpi = [2, 3, 4]
 
 # create DataFrame obiect with collums needed for kpi analysis
 df_korekty = df_korekty.iloc[:, df_korekty_needed_columns_for_kpi]
 
 # change data type for date in column 2
-df_korekty.iloc[:, 1] = pd.to_datetime(df_korekty.iloc[:, 1],dayfirst=True)
+df_korekty.iloc[:, 2] = pd.to_datetime(df_korekty.iloc[:, 2],dayfirst=True)
 
-print(df_rozliczenie_miesieczne.head())
-print("")
-print(df_korekty.head())
+# removing blank characters from column with index and batch number
+df_korekty.iloc[:, 1] = df_korekty.iloc[:, 1].apply(lambda x: str(x).strip())
+df_korekty.iloc[:, 1] = df_korekty.iloc[:, 1].apply(lambda x: str(x).strip()[-3:])
+df_korekty.iloc[:, 0] = df_korekty.iloc[:, 0].apply(lambda x: str(x).strip())
+df_korekty.iloc[:, 0] = df_korekty.iloc[:, 0].apply(lambda x: str(x).upper())
+
+
+# merge data frames df_rozliczenie_miesieczne and df_korekty by index and batch, save all rows from df_korekty (how='right')
+df_rozliczenie_miesieczne_df_korekty = pd.merge(df_rozliczenie_miesieczne, df_korekty, left_on=["INDEX", "NR.PRODUKCYJNY"], right_on=["index", "Nr partii"], how='right')
+
+
+# rename collumns
+df_rozliczenie_miesieczne_df_korekty.rename(columns={
+    'Lp': 'no',
+    'ilość ko-\nryg.mixów': 'if_corrected_on_the_extruder',
+    'NR.PRODUKCYJNY': 'batch_production',
+    'INDEX': 'index_production',
+    'KONEC PRODUCJI' : 'date_production',
+    'index': 'index_correction',
+    'Nr partii': 'batch_correction',
+    'Data kontroli': 'date_correction'
+}, inplace=True)
+
+
+
+# print(df_rozliczenie_miesieczne.head())
+# print("")
+# print(df_korekty.head())
+# print(df_rozliczenie_miesieczne_df_korekty)
 
 
 
@@ -104,3 +136,6 @@ print(df_korekty.head())
 # print("")
 # print("")
 # display_column_indices(df_rozliczenie_miesieczne)
+# print("")
+# print("")
+# display_column_indices(df_rozliczenie_miesieczne_df_korekty)
